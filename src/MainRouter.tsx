7fd3ba03feca0bbf79e0bdd986c95b8f1f41a918
SAIDI <mahrez.saidi@epsi.fr>
2023-10-23 12:22:29 +0200

    UserRoute

--- src/MainRouter.tsx
+++ src/MainRouter.tsx
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Landing from "./pages/Landing";
+import UserPage from "./pages/UserPage";
 
 function MainRouter() {
   return (
       <Router>
           <Routes>
               <Route path="/" element ={<Landing/>} />
+              <Route path="/" element ={<UserPage/>} />
           </Routes>
       </Router>
   );
